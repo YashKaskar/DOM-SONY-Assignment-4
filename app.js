4a83bb8e726c64ab3803f35dfb085d2846146ae9
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-12 11:20:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Webiste Name: [Sony](https://www.sony.co.in/)
+// ### Tasks
+
+//      change the button text To current Date.
+
+
+
+
+// Write your code here:
+
+document.querySelector('.container .btn-container');
+<div class="btn-container">…</div>
+
+document.querySelector('.container .btn-container').innerText = 'Tuesday, October 11, 2022';
+'Tuesday, October 11, 2022'
